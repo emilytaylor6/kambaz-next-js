944aed28976ace5c9792d35641aaeda93a99e3c0
Emily Taylor <emilytaylorr22@gmail.com>
2026-01-25 19:03:24 -0500

    Completed chapter 2.1: Worked with CSS styling in lab2.

---
+++ app/labs/lab2/Float.tsx
@@ -0,0 +1,98 @@
+import "./index.css";
+
+export default function Float() {
+    return (
+        <div id="wd-float-divs">
+            <h2>Float</h2>
+            <div>
+                <img className="wd-float-right"
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+                    Tell me, lover
+                    Now that you made your change
+                    Was your soul rediscovered?
+                    Was your heart rearranged?
+                    Are you still takin&apos; pills in the mornin&apos;?
+                    And did you lose that longin&apos; now
+                    For a walk through an ocean town?
+                    &apos;Cause this town&apos;s just an ocean now
+                    You don&apos;t hate the summers
+                    You&apos;re just afraid of the space
+                    Askin&apos; strangers for answers
+                    To forget what they say
+                    A boat beside a dock in the sunlight
+                    And nothin&apos; but the water and the sunrise now
+                    Just the lack of an open mouth
+                    &apos;Cause this town&apos;s just an ocean now
+                <img className="wd-float-left"
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+                    Bad
+                    I miss this place, your head and your heart
+                    And my dad
+                    Still tells me when they&apos;re playin&apos; your songs
+                    Laughin&apos; at the way that you would say
+                    &quot;If only, baby, there were cameras in the traffic lights
+                    They&apos;d make me a star
+                    They&apos;d make me a star&quot;
+                    I wanna go to Maine
+                    Mm, mm
+                    I wanna go to Maine
+                    Oh
+                    Bad
+                    I miss this place, your head and your heart
+                    And my dad
+                    Still tells me when they&apos;re playin&apos; your songs
+                    Laughin&apos; at the way that you would say
+                    &quot;If only, baby, there were cameras in the traffic lights
+                    They&apos;d make me a star
+                    They&apos;d make me a star&quot;
+                <img className="wd-float-right"
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+                    Hm, I wanna go to Maine (mm)
+                    Ooh-ooh, oh (mm)
+                    I wanna go to Maine (mm)
+                    Mm, God (mm)
+                    Tell me, lover
+                    Once you&apos;ve had a change of heart
+                    &apos;Cause we&apos;re no more than the fossils
+                    On Crescent Beach State Park, and we
+                    Used to sing along to church bells on Sundays
+                    And can you even hear &apos;em from the subway now?
+                    And I hope that we make you proud
+                    &apos;Cause this town&apos;s just an ocean now
+                <img className="wd-float-left"
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+                    I told you, love, if you could see it through my eyes
+                    You&apos;d understand my fear of everything in sight
+                    I know I&apos;d quickly sell my heart for some advice
+                    On how to ease a troubled mind
+                    My brain&apos;s a dog asleep that I cannot let lie
+                    Cloudy head, though I can&apos;t find a single silver lining
+                    Guess there is no hope for me in sight
+                    To ease this troubled mind
+                    I worry for the sun, yes, I worry for the snow
+                    I worry I&apos;ll die young while I worry I&apos;ll grow old
+                    Oh, I worry for the time I spent worrying alone
+                    It makes me sad
+                    It takes me back
+                    I break in half
+                    Ah-ooh, I break in half
+                    I took a bus out to the city where you live
+                    Eaves dropped on strangers&apos; conversations with their kids
+                    In hopes that maybe they&apos;d say something relevant
+                    To ease my worried head
+                <div className="wd-float-done"></div>
+            </div>
+            <div>
+            <div className="wd-float-left wd-dimension-portrait wd-bg-color-yellow">
+                Yellow </div>
+            <div className="wd-float-left wd-dimension-portrait wd-bg-color-blue wd-fg-color-white">
+                Blue </div>
+            <div className="wd-float-left wd-dimension-portrait wd-bg-color-red">
+                Red </div>
+            <img className="wd-float-right"
+                src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+            <div className="wd-float-done"></div>
+            </div>
+        </div>
+    );
+}
