d64ecec4f5f5c6eccf69e89dfe824458c0d7b53a
Emily Taylor <emilytaylorr22@gmail.com>
2026-03-15 22:59:50 -0400

    Added providers for labs to try to remove deployment errors.

--- app/labs/lab4/zustand/page.tsx
+++ app/labs/lab4/zustand/page.tsx
@@ -1,11 +1,16 @@
 import ZustandCounter from "./counter";
 import ZustandTodoList from "./todos/ZustandTodoList";
+import store from "../store";
+import { Provider } from "react-redux";
+
 export default function ZustandExamples() {
  return (
+  <Provider store={store}>
    <div>
      <h2>Zustand Examples</h2>
      <ZustandCounter />
      <ZustandTodoList />
    </div>
+   </Provider>
  );
 }
