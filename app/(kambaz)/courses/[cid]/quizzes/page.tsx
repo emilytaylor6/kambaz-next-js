8b1dd81f75d034c721a8f724e0abd60a84e00835
Emily Taylor <emilytaylorr22@gmail.com>
2026-04-13 22:11:35 -0400

    Added quiz screen and the display of each quiz in the list.

--- app/(kambaz)/courses/[cid]/quizzes/page.tsx
+++ app/(kambaz)/courses/[cid]/quizzes/page.tsx
@@ -1,7 +1,106 @@
+'use client'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { useParams } from "next/navigation"
+import { useEffect, useState } from "react";
+import * as client from "../../client";
+import { Col, ListGroup, ListGroupItem, Row } from "react-bootstrap";
+import { IoMdArrowDropdown, IoMdArrowDropright } from "react-icons/io";
+import { MdRocketLaunch } from "react-icons/md";
+import { useSelector } from "react-redux";
+import { RootState } from "@/app/(kambaz)/store";
+import Link from "next/link";
+import { produceDateAndTime, produceQuizAvailability } from "./utils";
+
 export default function Quizzes() {
+    const { cid } = useParams();
+    const { currentUser } = useSelector((state: RootState) => state.accountReducer);
+    const [quizzes, setQuizzes] = useState<any[]>([]);
+    const [collapsed, setCollapsed] = useState<boolean>(false);
+
+    const fetchQuizzes = async () => {
+        const quizzes = await client.findQuizzesForCourse(cid as string);
+        setQuizzes(quizzes);
+    }
+
+    useEffect(() => {
+        fetchQuizzes();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const produceAvailabilityComponent = (quiz: any) => {
+        const availability = produceQuizAvailability(quiz);
+        if (availability === "closed") return <b>Closed | </b>;
+        if (availability === "not available until") return (
+            <><b>Not available until</b> {produceDateAndTime(new Date(quiz.availableDate))} | </>
+        );
+        if (availability === "available until") return (
+            <><b>Available until</b> {produceDateAndTime(new Date(quiz.untilDate))} | </>
+        );
+        return "";
+    }
+
+    const produceDueDateComponent = (quiz: any) => {
+        if (!quiz.dueDate) return "";
+        return <><b>Due</b> {produceDateAndTime(new Date(quiz.dueDate))} | </>
+    }
+
+    const producePointsComponent = (quiz: any) => {
+        if (!quiz.points) return "";
+        return <span>{quiz.points} pt{quiz.points === 1 ? "" : "s"} | </span>
+    }
+
     return (
         <div>
-            Quizzes Placeholder
+            {/* add control buttons here */}
+            
+            <ListGroup className="rounded-0" id="wd-quizzes">
+                <ListGroupItem className="wd-quizzes-group p-0 mb-5 fs-5 border-gray">
+
+                    <div className="wd-title p-3 ps-2 bg-secondary">
+                        {collapsed ?
+                        <IoMdArrowDropright 
+                            className="me-2 fs-3" 
+                            onClick={() => setCollapsed(!collapsed)} 
+                        /> :
+                        <IoMdArrowDropdown 
+                            className="me-2 fs-3" 
+                            onClick={() => setCollapsed(!collapsed)}
+                        />}
+                        Assignment Quizzes
+                    </div>
+
+                    <ListGroup className="rounded-0" id={`wd-quizzes`}>
+                        {!collapsed && quizzes.map((quiz) => (
+                            <ListGroupItem key={quiz._id} className={`wd-quiz ${currentUser.role} p-2 ps-1`}>
+                                <Row>
+                                    <Col xs="auto" className="d-flex align-items-center ps-3 pe-0">
+                                        <MdRocketLaunch className={`ms-2 me-2 fs-3 wd-quiz-left-icon ${currentUser.role}`} />
+                                    </Col>
+
+                                    <Col>
+                                        <Link href={`/courses/${cid}/quizzes/${quiz._id}`} className="quiz-link">
+                                            {quiz.title}
+                                        </Link> 
+                                        <p className="fs-6 mb-0">
+                                            {produceAvailabilityComponent(quiz)}
+                                            {produceDueDateComponent(quiz)}
+                                            {producePointsComponent(quiz)}
+                                            {quiz.questionCount} Question{quiz.questionCount === 1 ? "" : "s"}
+                                        </p>
+                                    </Col>
+
+                                    <Col xs="auto" className="d-flex align-items-center">
+                                        {/* add individual control buttons */}
+                                    </Col>
+                                </Row>
+                            </ListGroupItem>
+                        ))}
+                    </ListGroup>
+
+                </ListGroupItem>
+            </ListGroup>
+
         </div>
     )
 }
---
+++ app/(kambaz)/courses/[cid]/quizzes/utils.ts
@@ -0,0 +1,49 @@
+  /**
+   * Used to produce a date and time in the format: MONTH DAY, YEAR at TIME with TIME being in the 
+   * 12 hour format with AM and PM, YEAR only applying if the given year is the same as the current
+   * year, and MONTH being a three character abbreviation
+   * @param date the given date
+   * @returns a string containing MONTH DAY, YEAR at TIME for the given date
+   */
+export const produceDateAndTime = (date: Date) => {
+    const currentDate = new Date();
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const time = date.toLocaleTimeString('en-US', { hour: 'numeric', minute: '2-digit' });
+    let dateString = "";
+    dateString = months[date.getMonth()] + " " + date.getDate().toString();
+    if (currentDate.getFullYear() !== date.getFullYear()) {
+      dateString = dateString +  ", " + date.getFullYear().toString();
+    }
+    dateString = dateString + " at " + time;
+    return dateString;
+}
+
+/**
+ * compares the given time and the current time 
+ * @param date the given date
+ * @returns negative when the given date comes before the current time, positive if after, and zero if same time
+ */
+export const compareTime = (date: Date) => {
+    const now = new Date();
+    return date.getTime() - now.getTime();
+}
+
+/**
+ * produces the availability status for the given quiz based on the current time
+ * @param quiz the given quiz
+ * @returns the availability status, either closed, available, available until, not available, or not available until as a string
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const produceQuizAvailability = (quiz: any) => {
+    if (!quiz.availableDate && !quiz.untilDate) return "available";
+
+    const availableComparison = quiz.availableDate ? compareTime(new Date(quiz.availableDate)) : -1;
+    const untilComparison = quiz.untilDate ? compareTime(new Date(quiz.untilDate)) : 1;
+
+    // past negative, future positive
+    if (untilComparison < 0) return "closed";
+    if (availableComparison > 0) return "not available until";
+    if (quiz.untilDate) return "available until";
+
+    return "available";
+}
